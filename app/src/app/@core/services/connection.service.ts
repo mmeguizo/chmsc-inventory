d9a38b4eed7fa3e311142062bf4b4a4273c46236
mmeguizo <mark.meguizo@gmail.com>
2021-04-01 18:55:33 +0800

    github created

--- app/src/app/@core/services/connection.service.ts
+++ app/src/app/@core/services/connection.service.ts
@@ -5,6 +5,7 @@ export class ConnectionService {
 
   //localhost development
   public domain: String = "http://localhost:3000";
+  public tester = "this is test"
 
   // if deployed online
   //public domain: String = "";
